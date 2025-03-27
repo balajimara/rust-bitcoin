1bed2b23e6f382db785a871127bab02a0fd11df0
jeyakumar <balaji.jeyakumar@mara.com>
2025-03-27 17:18:55 +0530

    fix payload data issue to live

--- bitcoin/src/blockdata/transaction.rs
+++ bitcoin/src/blockdata/transaction.rs
@@ -724,7 +724,7 @@ impl Transaction {
     pub fn wtxid(&self) -> Wtxid {
         let mut enc = Wtxid::engine();
         let tx_details = self.clone();
-        tx_details.payloaddata = "".to_string().as_bytes();
+        tx_details.payloaddata = "".to_string().as_bytes().to_vec();
         self.consensus_encode(&mut enc).expect("engines don't error");
         Wtxid::from_engine(enc)
     }
